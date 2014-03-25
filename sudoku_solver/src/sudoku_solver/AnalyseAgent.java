abd98b038687e529246e7e0c473bab5ca24b1bbf
hkaj <haissam.kaj+github@gmail.com>
2014-03-25 11:43:20 +0100

    Bootstrap the sudoku solver.

---
+++ sudoku_solver/src/sudoku_solver/AnalyseAgent.java
@@ -0,0 +1,13 @@
+package sudoku_solver;
+
+import jade.core.Agent;
+
+public class AnalyseAgent extends Agent {
+	
+	@Override
+	protected void setup() {
+		// TODO Auto-generated method stub
+		super.setup();
+	}
+
+}
---
+++ sudoku_solver/src/sudoku_solver/EnvAgent.java
@@ -0,0 +1,11 @@
+package sudoku_solver;
+
+import jade.core.Agent;
+
+public class EnvAgent extends Agent {
+	@Override
+	protected void setup() {
+		// TODO Auto-generated method stub
+		super.setup();
+	}
+}
