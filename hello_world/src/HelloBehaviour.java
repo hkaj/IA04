ca42bfb842b5d17f9e6f48808e9d40234e9cf113
Badmaul <chris.bellier92@gmail.com>
2014-03-26 22:36:26 +0100

    F****** warnings

--- hello_world/src/HelloBehaviour.java
+++ hello_world/src/HelloBehaviour.java
@@ -2,6 +2,7 @@ import jade.core.behaviours.Behaviour;
 import jade.core.Agent;
 
 // Behaviour de HelloAgent
+@SuppressWarnings("serial")
 public class HelloBehaviour extends Behaviour {
 	private String op;
 	
@@ -9,16 +10,16 @@ public class HelloBehaviour extends Behaviour {
 	public void set_op(String op) {
 		this.op = op;
 	}
-	// Constructeur surchargé pour prend l'opérateur en arg
+	// Constructeur surchargï¿½ pour prend l'opï¿½rateur en arg
 	public HelloBehaviour(Agent a, String op) {
 		super(a);
 		set_op(op);
 	}
-	// Action concrète de la behaviour
+	// Action concrï¿½te de la behaviour
 	public void action() {
 		System.out.println("Contact" + op);
 	}
-	// renseigne sur le succès du traitement
+	// renseigne sur le succï¿½s du traitement
 	public boolean done() {
 		return true;
 	}
--- hello_world/src/HelloWorldAgent.java
+++ hello_world/src/HelloWorldAgent.java
@@ -1,14 +1,15 @@
 import jade.core.Agent;
 
 // Agent du conteneur secondaire effectuant le traitement
+@SuppressWarnings("serial")
 public class HelloWorldAgent extends Agent {
-	// méthode orchestrant le comportement général de l'agent
+	// mï¿½thode orchestrant le comportement gï¿½nï¿½ral de l'agent
 	protected void setup() {
 		System.out.println("Hello World");
 		System.out.println(this.getName());
 		Object[] args = getArguments();
 		String op = (String) args[0];
-		// ajout de la behaviour à l'agent
+		// ajout de la behaviour ï¿½ l'agent
 		addBehaviour(new HelloBehaviour(this,op));
 	}
 }
