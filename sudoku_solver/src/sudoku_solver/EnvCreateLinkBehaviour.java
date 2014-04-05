dd5da989c74449c5897acdf8286731c00997c08b
Badmaul <chris.bellier92@gmail.com>
2014-04-05 23:10:06 +0200

    Links Behaviour process with one AnaAgent a time

--- sudoku_solver/src/sudoku_solver/EnvCreateLinkBehaviour.java
+++ sudoku_solver/src/sudoku_solver/EnvCreateLinkBehaviour.java
@@ -1,12 +1,5 @@
 package sudoku_solver;
 
-import java.util.Iterator;
-import java.util.List;
-
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 import jade.core.AID;
 import jade.core.Agent;
 import jade.core.behaviours.OneShotBehaviour;
@@ -25,21 +18,13 @@ public class EnvCreateLinkBehaviour extends OneShotBehaviour {
 
 	@Override
 	public void action() {
-		String content = m_listOfAnalAgentmessage.getContent();
-		ObjectMapper mapper = new ObjectMapper();
 		
-		try{
-			JsonNode jrootNode = mapper.readValue(content, JsonNode.class);
-			List<AID> listOfAgents = mapper.readValue(jrootNode.path("content").path("AIDs").textValue(), new TypeReference<List<AID>>(){});
-			EnvAgent agent = (EnvAgent) myAgent;
-			for(Iterator<AID> it = listOfAgents.iterator(); it.hasNext();){
-				agent.setConnectionAnalyseStructure(it.next());
-			}
-		}catch (Exception e){
-			e.printStackTrace();
-		}
-
-		myAgent.addBehaviour(new EnvSendRequestToAnalBehaviour(myAgent));
+		EnvAgent agent = (EnvAgent) myAgent;
+		AID anaAgentId = (AID) m_listOfAnalAgentmessage.getAllReplyTo().next();
+		agent.setConnectionAnalyseStructure(anaAgentId);
+		
+		//Envoi de la première requête d'envoi
+		myAgent.addBehaviour(new EnvSendRequestToAnalBehaviour(myAgent, anaAgentId));
 		
 	}
 	
