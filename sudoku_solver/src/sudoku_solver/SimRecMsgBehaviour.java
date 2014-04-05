f9290fb53af909f3cb2799dbadb7562b7d1e48b6
hkaj <haissam.kaj@gmail.com>
2014-04-05 18:21:45 +0200

    improve spec, add libs, implement anaAgent registration.

--- sudoku_solver/src/sudoku_solver/SimRecMsgBehaviour.java
+++ sudoku_solver/src/sudoku_solver/SimRecMsgBehaviour.java
@@ -2,22 +2,49 @@ package sudoku_solver;
 
 import java.util.ArrayList;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import jade.core.AID;
 import jade.core.Agent;
 import jade.core.behaviours.CyclicBehaviour;
+import jade.lang.acl.ACLMessage;
 
 public class SimRecMsgBehaviour extends CyclicBehaviour {
 	
 	public SimRecMsgBehaviour(SimulAgent agent) {
-		this.myAgent = (Agent) agent;
+		this.m_myAgent = agent;
+		this.m_envAgent = new AID("EnvAgent", true);
+		
 	}
 
 	@Override
 	public void action() {
-		// TODO Auto-generated method stub
+		ACLMessage order = myAgent.receive();
+		if (order != null) {
+			if (order.getPerformative() == ACLMessage.SUBSCRIBE) {
+				// register the analysis agents
+				String content = order.getContent();
+				ObjectMapper mapper = new ObjectMapper();
+				try {
+					JsonNode jrootNode = mapper.readValue(content, JsonNode.class);
+					AID agentAID = new AID(jrootNode.path("content").path("AID").path(0).textValue(), true);
+					m_anaAgents.add(agentAID);
+				} catch (Exception ex) {
+					ex.printStackTrace();
+				}
+			} else if (order.getPerformative() == ACLMessage.INFORM) {
+				// handle the analysis agents responses
+			} else {
+				System.out.println("SimulAgent received an unexpected message");
+			}
+		}
 
 	}
 	
 	// Members
-	ArrayList<AnaAgent> m_anaAgents; 
+	ArrayList<AID> m_anaAgents;
+	AID m_envAgent;
+	SimulAgent m_myAgent;
 
 }
