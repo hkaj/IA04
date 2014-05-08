3230c7ce5da5beb93c3fdacaa86f692000abfe99
hkaj <haissam.kaj@gmail.com>
2014-05-08 22:49:45 +0200

    Build the 3 queries and a test env for TD6/part1.

---
+++ ontologie/src/ontologie/SparqlMain.java
@@ -0,0 +1,47 @@
+package ontologie;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+import com.hp.hpl.jena.query.Query;
+import com.hp.hpl.jena.query.QueryExecution;
+import com.hp.hpl.jena.query.QueryExecutionFactory;
+import com.hp.hpl.jena.query.QueryFactory;
+import com.hp.hpl.jena.query.ResultSet;
+import com.hp.hpl.jena.query.ResultSetFormatter;
+import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
+
+public class SparqlMain {
+
+	public SparqlMain() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		String[] queries = {"ressources/q11.sparql", "ressources/q12.sparql", "ressources/q13.sparql"};
+		for (String q: queries) {
+			System.out.println("Executing the query in '" + q + "'...");
+			test(q);
+		}
+
+	}
+	
+	public static void test(String queryFile) {
+		Model model = ModelFactory.createDefaultModel();
+		try {
+			model.read(new FileInputStream("ressources/foaf.n3"), null, "TURTLE");
+			runExecQuery(queryFile, model);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void runExecQuery(String qfilename, Model model) {
+		Query query = QueryFactory.read(qfilename);
+		QueryExecution queryExecution = QueryExecutionFactory.create(query, model);
+		ResultSet r = queryExecution.execSelect();
+		ResultSetFormatter.out(System.out, r);
+		queryExecution.close();
+	}
+}
